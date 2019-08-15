change logging approach for better time complexity... + made sure to use the fixed gumbel-softmax function. TODO: test with basic (Havrylov et al.'s) agents. + implemented STGT approach over the obverter agent in order to study whether the best of both world can be achieved.

ReferentialGym/agents/listener.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import copy 
 
 from ..networks import layer_init
+from ..utils import gumbel_softmax 
 
 
 class Listener(nn.Module):
@@ -22,6 +23,7 @@ class Listener(nn.Module):
         self.max_sentence_length = max_sentence_length
         self.agent_id = agent_id
         self.logger = logger 
+        self.log_idx = 0
 
         # Multi-round:
         self._reset_rnn_states()
@@ -51,9 +53,13 @@ class Listener(nn.Module):
         if self.logger is None: 
             return 
 
+        agent_log_dict = {f"{self.agent_id}": dict()}
         for key, data in log_dict.items():
-            logdict = { f"{self.agent_id}": {f"{key}":data}}
-            self.logger.add_dict(logdict, batch=True) 
+            agent_log_dict[f"{self.agent_id}"].update({f"{key}":data})
+        
+        self.logger.add_dict(agent_log_dict, batch=True, idx=self.log_idx) 
+        
+        self.log_idx += 1
 
     def _tidyup(self):
         pass 
@@ -132,10 +138,21 @@ class Listener(nn.Module):
             if self.training:
                 if 'gumbel_softmax' in graphtype:
                     self.tau = self._compute_tau(tau0=tau0)
-                    tau = self.tau.view((-1,1,1)).repeat(1,self.max_sentence_length,self.vocab_size)
-                
-                    straight_through = (graphtype == 'straight_through_gumbel_softmax')
-                    next_sentences = nn.functional.gumbel_softmax(logits=next_sentences_logits, tau=tau, hard=straight_through, dim=-1)
+                    #tau = self.tau.view((-1,1,1)).repeat(1,1,self.vocab_size)
+                    tau = self.tau.view((-1))
+
+                    straight_through = ('straight_through_gumbel_softmax' in graphtype)
+                    
+                    next_sentences_stgs = []
+                    for bidx in range(len(next_sentences_logits)):
+                        nsl_in = next_sentences_logits[bidx]
+                        tau_in = tau[bidx]
+                        next_sentences_stgs.append( gumbel_softmax(logits=nsl_in, tau=tau_in, hard=straight_through, dim=-1))
+                        #next_sentences_stgs.append( nn.functional.gumbel_softmax(logits=nsl_in, tau=tau_in, hard=straight_through, dim=-1))
+                    next_sentences = next_sentences_stgs
+                    if isinstance(next_sentences, list): 
+                        next_sentences = nn.utils.rnn.pad_sequence(next_sentences, batch_first=True, padding_value=0.0).float()
+                        # (batch_size, max_sentence_length<=max_sentence_length, vocab_size)
         
         output_dict = {'decision': decision_logits, 
                        'sentences_widx':next_sentences_widx, 

ReferentialGym/agents/obverter_agent.py
@@ -54,6 +54,8 @@ class ObverterAgent(Listener):
         self.symbol_encoder = nn.Linear(self.vocab_size, self.kwargs['symbol_processing_nbr_hidden_units'], bias=False)
         self.symbol_decoder = nn.Linear(self.kwargs['symbol_processing_nbr_hidden_units'], self.vocab_size)
         
+        self.tau_fc = layer_init(nn.Linear(self.kwargs['temporal_encoder_nbr_hidden_units'], 1 , bias=False))
+        
         self.reset()
 
     def reset(self):
@@ -64,6 +66,10 @@ class ObverterAgent(Listener):
 
     def _tidyup(self):
         self.embedding_tf_final_outputs = None
+
+    def _compute_tau(self, tau0):
+        invtau = tau0 + torch.log(1+torch.exp(self.tau_fc(self.embedding_tf_final_outputs))).squeeze()
+        return 1.0/invtau
 
     def _sense(self, experiences, sentences=None):
         """
@@ -309,13 +315,13 @@ class ObverterAgent(Listener):
                     decision_probs = F.softmax(decision_logits, dim=1)
                 # (batch_size=vocab_size, (nbr_distractors+1) )
                                     
-                target_decision_probs_per_vocab = decision_probs[:,btarget_idx].cpu()
+                target_decision_probs_per_vocab = decision_probs[:,btarget_idx]#.cpu()
                 # (batch_size=vocab_size, )
                 vocab_idx_op, vocab_idx_argop = operation(target_decision_probs_per_vocab, dim=0)
                 # (batch_size=vocab_size, )
                 
                 sentences_widx[b].append( vocab_idx_argop)
-                sentences_logits[b].append( target_decision_probs_per_vocab.view((1,1,-1)))
+                sentences_logits[b].append( target_decision_probs_per_vocab.view((1,-1)))
                 sentences_one_hots[b].append( nn.functional.one_hot(vocab_idx_argop, num_classes=vocab_size).view((1,-1)))
                 
                 # next rnn_states:
@@ -332,17 +338,15 @@ class ObverterAgent(Listener):
             # Embed the sentence:
             sentences_widx[b] = torch.cat([ torch.FloatTensor([word_idx]).view((1,1,-1)) for word_idx in sentences_widx[b]], dim=1)
             # (batch_size=1, sentence_length<=max_sentence_length, 1)
-            sentences_logits[b] = torch.cat(sentences_logits[b], dim=1)
-            # (batch_size=1, sentence_length<=max_sentence_length, vocab_size)
+            sentences_logits[b] = torch.cat(sentences_logits[b], dim=0)
+            # (sentence_length<=max_sentence_length, vocab_size)
             sentences_one_hots[b] = torch.cat(sentences_one_hots[b], dim=0) 
-            # (batch_size=1, sentence_length<=max_sentence_length, vocab_size)
+            # (sentence_length<=max_sentence_length, vocab_size)
 
             # Reset the state for the next sentence generation in the batch:
             states = init_rnn_states
 
         sentences_one_hots = nn.utils.rnn.pad_sequence(sentences_one_hots, batch_first=True, padding_value=0.0).float()
         # (batch_size=1, max_sentence_length<=max_sentence_length, vocab_size)
-
-        if features_embedding.is_cuda: sentences_one_hots = sentences_one_hots.cuda()
 
         return sentences_widx, sentences_logits, sentences_one_hots