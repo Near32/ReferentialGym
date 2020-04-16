reformated iterated learning scheme in agent and populationa handler module. + fixed an issue with the embedding tf final output computation in obverter. + made the lstm cnn listener more in touch with the data it receives than with its own default config, in order to introduce different number of distractors at train and testing times. TODO: debug Sort-of-CLEVR remainderToUse strategy: the train and test set are inversed when the value is different than 0? made the multi head classifiction module able to use only one head for every input-target pair it may have, as an option for relational reasoning datasets... + added obverter training scheme in the relational reasoning study. + made the study use same_head hyperparameter for multi head classification module in order to enable the analogy testing schemes.

ReferentialGym/agents/agent.py
@@ -450,13 +450,6 @@ class Agent(Module):
 
         if 'listener' in self.role:
             sample = input_streams_dict['sample']
-            if config['iterated_learning_scheme']:
-                listener_speaking_outputs = self(experiences=sample['speaker_experiences'], 
-                                                 sentences=None, 
-                                                 multi_round=input_streams_dict['multi_round'],
-                                                 graphtype=input_streams_dict['graphtype'],
-                                                 tau0=input_streams_dict['tau0'])
-            
             
             decision_logits = outputs_dict['decision']
             final_decision_logits = decision_logits
@@ -747,16 +740,37 @@ class Agent(Module):
             
             outputs_dict['decision_probs'] = decision_probs
             
-            if 'iterated_learning_scheme' in config and config['iterated_learning_scheme']:
+            if 'iterated_learning_scheme' in config \
+                and config['iterated_learning_scheme']\
+                and 'iterated_learning_rehearse_MDL' in config \
+                and config['iterated_learning_rehearse_MDL']:
+                # Rehearsing:
+                listener_speaking_outputs = self(experiences=sample['speaker_experiences'], 
+                                                 sentences=None, 
+                                                 multi_round=input_streams_dict['multi_round'],
+                                                 graphtype=input_streams_dict['graphtype'],
+                                                 tau0=input_streams_dict['tau0'])
                 # Let us enforce the Minimum Description Length Principle:
                 # Listener's speaking entropy:
+                listener_sentences_log_probs = [s_logits.reshape(-1,self.vocab_size).log_softmax(dim=-1) for s_logits in listener_speaking_outputs['sentences_logits']]
+                listener_sentences_log_probs = torch.cat(
+                    [s_log_probs.gather(dim=-1,index=s_widx[:s_log_probs.shape[0]].long()).sum().unsqueeze(0) 
+                    for s_log_probs, s_widx in zip(listener_sentences_log_probs, listener_speaking_outputs['sentences_widx'])], 
+                    dim=0)
+                listener_entropies_per_sentence = -(listener_sentences_log_probs.exp() * listener_sentences_log_probs)
+                # (batch_size, )
+                # Maximization:
+                losses_dict['ilm_MDL_loss'] = [-config['iterated_learning_rehearse_MDL_factor'], listener_entropies_per_sentence]
+
+                '''
                 listener_speaking_entropies = [torch.cat([ torch.distributions.bernoulli.Bernoulli(logits=w_logits).entropy().mean().view(-1) for w_logits in s_logits], dim=0) for s_logits in listener_speaking_outputs['sentences_logits']]
                 # List of size batch_size of Tensor of shape (sentence_length,)
                 per_sentence_max_entropies = torch.stack([ lss.max(dim=0)[0] for lss in listener_speaking_entropies])
                 # Tensor of shape (batch_size,1)
                 ilm_loss = per_sentence_max_entropies.mean(dim=-1)
                 # (batch_size, )
-                losses_dict['ilm_loss'] = [1.0, ilm_loss]            
+                losses_dict['ilm_MDL_loss'] = [1.0, ilm_loss]
+                '''
 
             if 'with_listener_entropy_regularization' in config and config['with_listener_entropy_regularization']:
                 entropies = torch.cat([ torch.distributions.categorical.Categorical(logits=d_logits).entropy().view(1) for d_logits in final_decision_logits], dim=-1)

ReferentialGym/agents/differentiable_obverter_agent.py
@@ -45,20 +45,20 @@ class DifferentiableObverterAgent(Listener):
         self.differentiable = differentiable
         if not(self.differentiable):
 
-            del self.input_stream_ids['speaker']['current_listener:sentences_one_hot']
-            del self.input_stream_ids['speaker']['current_listener:sentences_widx']
+            del self.input_stream_ids['speaker']['modules:current_listener:sentences_one_hot']
+            del self.input_stream_ids['speaker']['modules:current_listener:sentences_widx']
 
-            del self.input_stream_ids['listener']['current_speaker:sentences_one_hot']
-            del self.input_stream_ids['listener']['current_speaker:sentences_widx']
+            del self.input_stream_ids['listener']['modules:current_speaker:sentences_one_hot']
+            del self.input_stream_ids['listener']['modules:current_speaker:sentences_widx']
 
             self.input_stream_ids['speaker'].update({
-                'current_listener:sentences_one_hot.detach':'sentences_one_hot',
-                'current_listener:sentences_widx.detach':'sentences_widx',
+                'modules:current_listener:sentences_one_hot.detach':'sentences_one_hot',
+                'modules:current_listener:sentences_widx.detach':'sentences_widx',
             })
 
             self.input_stream_ids['listener'].update({
-                'current_speaker:sentences_one_hot.detach':'sentences_one_hot',
-                'current_speaker:sentences_widx.detach':'sentences_widx', 
+                'modules:current_speaker:sentences_one_hot.detach':'sentences_one_hot',
+                'modules:current_speaker:sentences_widx.detach':'sentences_widx', 
             })
         
 
@@ -139,7 +139,10 @@ class DifferentiableObverterAgent(Listener):
                 )
         else:
             self.temporal_feature_encoder = None
-            assert(self.kwargs['temporal_encoder_nbr_hidden_units'] == self.kwargs['nbr_stimulus']*self.encoder_feature_shape)
+            print("WARNING: Symbol processing :: the number of hidden units is being reparameterized to fit to convolutional features.")
+            self.kwargs['cnn_encoder_feature_dim'] = self.encoder_feature_shape
+            self.kwargs['temporal_encoder_nbr_hidden_units'] = self.kwargs['nbr_stimulus']*self.kwargs['cnn_encoder_feature_dim']
+            self.kwargs['symbol_processing_nbr_hidden_units'] = self.kwargs['temporal_encoder_nbr_hidden_units']
 
         self.normalization = nn.BatchNorm1d(num_features=self.kwargs['temporal_encoder_nbr_hidden_units'])
         
@@ -202,17 +205,12 @@ class DifferentiableObverterAgent(Listener):
 
     def _tidyup(self):
         self.embedding_tf_final_outputs = None
+        
         if isinstance(self.cnn_encoder, BetaVAE):
             self.VAE_losses = list()
             self.compactness_losses.clear()
             self.buffer_cnn_output_dict = dict()
 
-    '''
-    def _compute_tau(self, tau0, emb=None):
-        if emb is None: emb = self.embedding_tf_final_outputs
-        invtau = tau0 + torch.log(1+torch.exp(self.tau_fc(emb))).squeeze()
-        return 1.0/invtau
-    '''
     def _compute_tau(self, tau0, h):
         invtau = 1.0 / (self.tau_fc(h).squeeze() + tau0)
         return invtau
@@ -229,6 +227,7 @@ class DifferentiableObverterAgent(Listener):
             features: Tensor of shape `(batch_size, -1, nbr_stimulus, feature_dim).
         """
         batch_size = experiences.size(0)
+        nbr_distractors_po = experiences.size(1)
         experiences = experiences.view(-1, *(experiences.size()[3:]))
         features = []
         feat_maps = []
@@ -269,13 +268,10 @@ class DifferentiableObverterAgent(Listener):
             features.append(featout)
             feat_maps.append(feat_map)
 
-        features = self.cnn_encoder_normalization(torch.cat(features, dim=0))
+        self.features = self.cnn_encoder_normalization(torch.cat(features, dim=0))
         self.feat_maps = torch.cat(feat_maps, dim=0)
         
-        if self.use_feat_converter:
-            features = features.view(batch_size, -1, self.kwargs['nbr_stimulus'], self.encoder_feature_shape)
-        else:
-            features = features.view(batch_size, -1, self.kwargs['nbr_stimulus'], self.kwargs['cnn_encoder_feature_dim'])    
+        self.features = self.features.view(batch_size, nbr_distractors_po, self.config['nbr_stimulus'], -1)
         # (batch_size, nbr_distractors+1 / ? (descriptive mode depends on the role of the agent), nbr_stimulus, feature_dim)
         
         if isinstance(self.cnn_encoder, BetaVAE):
@@ -288,7 +284,7 @@ class DifferentiableObverterAgent(Listener):
             if len(self.compactness_losses):
                 self.log_dict['unsup_compactness_loss'] = torch.cat(self.compactness_losses).mean()
 
-        return features 
+        return self.features
 
     def _reason(self, sentences, features):
         """
@@ -413,24 +409,7 @@ class DifferentiableObverterAgent(Listener):
         # (batch_size, nbr_distractors+1 / ? (descriptive mode depends on the role of the agent), nbr_stimulus, cnn_encoder_feature_dim)
         
         # Forward pass:
-        if self.embedding_tf_final_outputs is None: 
-            '''
-            features = features.view(-1, *(features.size()[2:]))
-            # (batch_size*(nbr_distractors+1) / ? (descriptive mode depends on the role of the agent), nbr_stimulus, kwargs['cnn_encoder_feature_dim'])
-            rnn_outputs = []
-            total_size = features.size(0)
-            mini_batch_size = min(self.kwargs['temporal_encoder_mini_batch_size'], total_size)
-            for featin in torch.split(features, split_size_or_sections=mini_batch_size, dim=0):
-                outputs, _ = self.temporal_feature_encoder(featin)
-                # (mini_batch_size, -1, kwargs['temporal_encoder_nbr_hidden_units'])
-                rnn_outputs.append( outputs)
-            outputs = torch.cat(rnn_outputs, dim=0)
-            outputs = outputs.view(batch_size, -1, self.kwargs['nbr_stimulus'], self.kwargs['temporal_encoder_nbr_hidden_units'])
-        
-            embedding_tf_final_outputs = outputs[:,:,-1,:].contiguous()
-            self.embedding_tf_final_outputs = embedding_tf_final_outputs.view(batch_size, -1)
-            # (batch_size, (nbr_distractors+1) / ? (descriptive mode depends on the role of the agent) * kwargs['temporal_encoder_nbr_hidden_units'])
-            '''
+        if self.embedding_tf_final_outputs is None or self.embedding_tf_final_outputs.shape[1] != nbr_distractors_po: 
             if self.temporal_feature_encoder:
                 features = features.view(-1, *(features.size()[2:]))
                 # (batch_size*(nbr_distractors+1), nbr_stimulus, kwargs['cnn_encoder_feature_dim'])
@@ -450,11 +429,11 @@ class DifferentiableObverterAgent(Listener):
                 embedding_tf_final_outputs = outputs[:,0,-1,:].contiguous()
                 # (batch_size, kwargs['temporal_encoder_feature_dim'])
                 self.embedding_tf_final_outputs = self.normalization(embedding_tf_final_outputs.reshape((-1, self.kwargs['temporal_encoder_nbr_hidden_units'])))
-                self.embedding_tf_final_outputs = self.embedding_tf_final_outputs.reshape(batch_size, self.kwargs['nbr_distractors']+1, -1)
+                self.embedding_tf_final_outputs = self.embedding_tf_final_outputs.reshape(batch_size, nbr_distractors_po, -1)
                 # (batch_size, 1, kwargs['temporal_encoder_nbr_hidden_units'])
             else:
                 self.embedding_tf_final_outputs = self.normalization(features.reshape((-1, self.kwargs['temporal_encoder_nbr_hidden_units'])))
-                self.embedding_tf_final_outputs = self.embedding_tf_final_outputs.reshape((batch_size, self.kwargs['nbr_distractors']+1, -1))
+                self.embedding_tf_final_outputs = self.embedding_tf_final_outputs.reshape((batch_size, nbr_distractors_po, -1))
                 # (batch_size, 1, kwargs['temporal_encoder_nbr_hidden_units'])
 
         # No need to consume the sentences:

ReferentialGym/datasets/extended_sort_of_CLEVR_dataset.py
@@ -407,22 +407,6 @@ def generate_dataset(root,
             dataset[f"non_relational_qs_{subtype_id}"].append(np.stack(datapoint[2][0][subtype_id]))
             dataset[f"non_relational_as_{subtype_id}"].append(np.stack(datapoint[2][1][subtype_id]))
 
-        '''
-        dataset['relational_qs_0'].append(np.stack(datapoint[1][0][0]))
-        dataset['relational_qs_1'].append(np.stack(datapoint[1][0][1]))
-        dataset['relational_qs_2'].append(np.stack(datapoint[1][0][2]))
-        dataset['non_relational_qs_0'].append(np.stack(datapoint[2][0][0]))
-        dataset['non_relational_qs_1'].append(np.stack(datapoint[2][0][1]))
-        dataset['non_relational_qs_2'].append(np.stack(datapoint[2][0][2]))
-        
-        dataset['relational_as_0'].append(np.asarray(datapoint[1][1][0]))
-        dataset['relational_as_1'].append(np.asarray(datapoint[1][1][1]))
-        dataset['relational_as_2'].append(np.asarray(datapoint[1][1][2]))
-        dataset['non_relational_as_0'].append(np.asarray(datapoint[2][1][0]))
-        dataset['non_relational_as_1'].append(np.asarray(datapoint[2][1][1]))
-        dataset['non_relational_as_2'].append(np.asarray(datapoint[2][1][2]))
-        '''
-
     print('saving dataset...')
     filename = os.path.join(dirs,'xsort-of-clevr.pickle')
     with  open(filename, 'wb') as f:
@@ -473,7 +457,7 @@ class XSortOfCLEVRDataset(Dataset):
                                        nb_nr_qs=self.nb_nr_qs,
                                        nb_r_qs=self.nb_r_qs)
             else:
-                raise RuntimeError('Dataset not found. You can use download=True to download it')
+                raise RuntimeError('Dataset not found.')
         else:
             filepath = os.path.join(self.root, self.file)
             with open(filepath, 'rb') as f:
@@ -593,10 +577,12 @@ class XSortOfCLEVRDataset(Dataset):
                 
         relational_questions = {f"relational_questions_{k}":torch.from_numpy(v[idx]).float() for k,v in self.relational_qs.items()}
         non_relational_questions = {f"non_relational_questions_{k}":torch.from_numpy(v[idx]).float() for k,v in self.non_relational_qs.items()}
+        #(nbr_objects x qst_repr=15)
         
         relational_answers = {f"relational_answers_{k}":torch.from_numpy(v[idx]).long() for k,v in self.relational_as.items()}
         non_relational_answers = {f"non_relational_answers_{k}":torch.from_numpy(v[idx]).long() for k,v in self.non_relational_as.items()}
-        
+        #(nbr_objects x ans_repr=1)
+
         # Do we test the analogy on the color/object_id?
         if self.test_id_analogy:
             # Let us reserve the QAs with regard to color/object_id greater than the given threshold:

ReferentialGym/modules/multi_head_classification_module.py
@@ -14,18 +14,21 @@ def build_MultiHeadClassificationModule(id:str,
     
     # Multi Heads:
     # Add the feature map size as input to the architectures:
-    input_shape = config['input_shape']
+    input_shapes = config['input_shapes']
     for idx in range(len(config['heads_archs'])):
-        config['heads_archs'][idx] = [input_shape]+config['heads_archs'][idx]
-    
+        config['heads_archs'][idx] = [input_shapes[idx]]+config['heads_archs'][idx]
+        if config['same_head']:
+            break
+
     # Make sure there are as many heads as proposed architectures:
     while len(config['heads_archs']) != len(config['heads_output_sizes']):
         config['heads_archs'].append(copy.deepcopy(config['heads_archs'][-1]))
 
     # Add output sizes to the archs:
     for idx, output_size in enumerate(config['heads_output_sizes']):
-        if isinstance(output_size, int):
-            config['heads_archs'][idx].append(output_size)
+        config['heads_archs'][idx].append(output_size)
+        if config['same_head']:
+            break
 
 
     heads = nn.ModuleList()
@@ -58,13 +61,16 @@ def build_MultiHeadClassificationModule(id:str,
         else:
             layer = None 
         heads.append(layer)
-    
+        
+        if config['same_head']:
+            break
+
     module = MultiHeadClassificationModule(id=id,
                                            config=config,
                                            heads=heads,
                                            input_stream_ids=input_stream_ids)
     print(module)
-    
+
     return module
 
 
@@ -76,11 +82,11 @@ class MultiHeadClassificationModule(Module):
                  input_stream_ids:Dict[str,str],
                  final_fn:nn.Module=nn.Softmax(dim=-1)):
 
-        assert "inputs" in input_stream_ids.values(),\
-               "ClassificationModule relies on 'inputs' id to start its pipeline.\n\
+        assert "inputs_0" in input_stream_ids.values(),\
+               "ClassificationModule relies on, at least, 'input_0' id to start its pipeline.\n\
                 Not found in input_stream_ids."
-        assert "targets" in input_stream_ids.values(),\
-               "ClassificationModule relies on 'targets' id to compute its pipeline.\n\
+        assert "targets_0" in input_stream_ids.values(),\
+               "ClassificationModule relies on, at least, 'targets_0' id to compute its pipeline.\n\
                 Not found in input_stream_ids."
         assert "losses_dict" in input_stream_ids.values(),\
                "ClassificationModule relies on 'losses_dict' id to record the computated losses.\n\
@@ -88,13 +94,13 @@ class MultiHeadClassificationModule(Module):
         assert "logs_dict" in input_stream_ids.values(),\
                "ClassificationModule relies on 'logs_dict' id to record the accuracies.\n\
                 Not found in input_stream_ids."
-        assert "loss_id" in config.keys(),\
-               "ClassificationModule relies on 'loss_id' key to record the computated losses and accuracies.\n\
-                Not found in config keys."
         assert "mode" in input_stream_ids.values(),\
                "ClassificationModule relies on 'mode' key to record the computated losses and accuracies.\n\
                 Not found in input_stream_ids."
-
+        assert "loss_ids" in config.keys(),\
+               "ClassificationModule relies on 'loss_ids' key to record the computated losses and accuracies.\n\
+                Not found in config keys."
+        
         super(MultiHeadClassificationModule, self).__init__(id=id, 
             type="MultiHeadClassificationModule", 
             config=config, 
@@ -121,67 +127,65 @@ class MultiHeadClassificationModule(Module):
         outputs_stream_dict = {}
 
         mode = input_streams_dict['mode']
-        inputs = input_streams_dict['inputs']
+
+        inputs = {k:v for k,v in input_streams_dict.items() if 'inputs' in k}
+        nbr_inputs = len(inputs)
         
-        shape_inputs = inputs.shape
-        batch_size = shape_inputs[0]
+        shape_inputs = {k:v.shape for k,v in inputs.items()}
+        batch_sizes = {k:sh[0] for k,sh in shape_inputs.items()}
         
-        need_reshaping = False
-        if len(shape_inputs) > 2:
-            '''
-            Let us reshape the input so that it only has 2 dimensions.
-            Afterwards, the resulting outputs will be reshaped and 
-            averaged over the extra dimensions.
-            N.B: the target is reshaped similarly...
-            '''
-            need_reshaping = True
-            last_dim_size = shape_inputs[-1]
-            inputs = inputs.reshape(-1, last_dim_size)
+        need_reshaping = {k:False for k in inputs}
+        for k, input_shape in shape_inputs.items():
+            if len(input_shape) > 2:
+                '''
+                Let us reshape the input so that it only has 2 dimensions.
+                Afterwards, the resulting outputs will be reshaped and 
+                averaged over the extra dimensions.
+                N.B: the target is reshaped similarly...
+                '''
+                need_reshaping[k] = True
+                last_dim_size = input_shape[-1]
+                inputs[k] = inputs[k].reshape(-1, last_dim_size)
 
-        if self.config['detach_input']:
-            inputs = inputs.detach()
+        losses = {}
+        accuracies = {}
+        for ii, (key,inp) in enumerate(inputs.items()):
+            if self.config['same_head']:    ih = 0
+            else:   ih = ii
+            head = self.heads[ih]
 
-        losses = []
-        accuracies = []
-        for ih, head in enumerate(self.heads):
-            if isinstance(self.config['heads_output_sizes'][ih], int):
-                head_output = head(inputs)
-                final_output = self.final_fn(head_output)
+            head_output = head(inp)
+            final_output = self.final_fn(head_output)
 
-                # Loss:
-                target_idx = input_streams_dict['targets']
-                if need_reshaping:
-                    target_idx = target_idx.reshape(-1, len(self.heads))
+            # Loss:
+            target_idx = input_streams_dict[f'targets_{ii}']
+            if need_reshaping[key]:
+                # Target indices corresponds to 1 per batch element:
+                target_idx = target_idx.reshape(-1)          
+            
+            criterion = nn.CrossEntropyLoss(reduction='none')
+            loss = criterion( final_output, target_idx.squeeze().long())
 
-                target_idx = target_idx[..., ih]                
-                
-                criterion = nn.CrossEntropyLoss(reduction='none')
-                loss = criterion( final_output, target_idx.squeeze().long())
+            # Accuracy:
+            argmax_final_output = final_output.argmax(dim=-1)
+            accuracy = 100.0*(target_idx==argmax_final_output).float().mean()
 
-                # Accuracy:
-                argmax_final_output = final_output.argmax(dim=-1)
-                accuracy = 100.0*(target_idx==argmax_final_output).float().mean()
-
-                if need_reshaping:
-                    loss = loss.reshape(batch_size,-1).mean(-1)
-            else:
-                loss = torch.zeros(batch_size).to(flatten_input.device)
-                accuracy = torch.zeros(1)
-
-            losses.append(loss)
-            accuracies.append(accuracy)
+            if need_reshaping[key]:
+                loss = loss.reshape(batch_sizes[key],-1).mean(-1)
+        
+            losses[key] = loss
+            accuracies[key] = accuracy
 
         losses_dict = input_streams_dict['losses_dict']
         logs_dict = input_streams_dict['logs_dict'] 
         
         # MultiHead Losses:
-        for idx, loss in enumerate(losses):
-            #losses_dict[f"{mode}/{self.config['loss_id']}/multi_head_{idx}_loss"] = [1.0, loss]
-            losses_dict[f"{self.config['loss_id']}/multi_head_{idx}_loss"] = [1.0, loss]
+        for key, loss in losses.items():
+            losses_dict[f"{self.config['loss_ids'][key]}/loss"] = [1.0, loss]
 
         # MultiHead Accuracy:
-        for idx, acc in enumerate(accuracies):
-            logs_dict[f"{mode}/{self.config['loss_id']}/multi_head_{idx}_accuracy"] = acc
+        for key, acc in accuracies.items():
+            logs_dict[f"{mode}/{self.config['loss_ids'][key]}/accuracy"] = acc
 
         outputs_stream_dict['losses'] = losses
         outputs_stream_dict['accuracies'] = accuracies