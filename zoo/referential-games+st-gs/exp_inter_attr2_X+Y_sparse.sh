dba21e7987824603a078813c877a5782b1f48a3e
Kevin Denamganai <denamganai.kevin@gmail.com>
2020-05-18 18:27:36 +0100

    made vocab size parameterizable in launch scripts...

--- zoo/referential-games+st-gs/exp_inter_attr2_X+Y_sparse.sh
+++ zoo/referential-games+st-gs/exp_inter_attr2_X+Y_sparse.sh
@@ -3,18 +3,19 @@
 # $2==ARCH( /BN+CNN3x3)
 # $3==NBR_DISTRACTORS_TRAIN (48 train + 16 test)
 # $4==NBR_DISTRACTORS_TEST (48 train + 16 test)
-# $5==BATCH_SIZE (2/12/24/36/48)
+# $5==VOCAV_SIZE (5/20)
+# $6==BATCH_SIZE (2/12/24/36/48)
 
 python train.py --seed $(($1+0)) \
 --arch $2 \
 --epoch 10000 \
 --distractor_sampling uniform \
 --nbr_train_distractors $3 --nbr_test_distractors $4 \
---max_sentence_length 20 --vocab_size 100 \
+--max_sentence_length $5 --vocab_size 100 \
 --dataset dSprites \
 --train_test_split_strategy combinatorial2-Y-4-2-X-4-2-Orientation-40-N-Scale-6-N-Shape-3-N \
 --use_cuda \
---batch_size $5 &
+--batch_size $6 &
 #--shared_architecture \
 #--fast \
 
@@ -24,11 +25,11 @@ python train.py --seed $(($1+10)) \
 --epoch 10000 \
 --distractor_sampling uniform \
 --nbr_train_distractors $3 --nbr_test_distractors $4 \
---max_sentence_length 20 --vocab_size 100 \
+--max_sentence_length $5 --vocab_size 100 \
 --dataset dSprites \
 --train_test_split_strategy combinatorial2-Y-4-2-X-4-2-Orientation-40-N-Scale-6-N-Shape-3-N \
 --use_cuda \
---batch_size $5 &
+--batch_size $6 &
 #--shared_architecture \
 #--fast \
 
@@ -38,11 +39,11 @@ python train.py --seed $(($1+20)) \
 --epoch 10000 \
 --distractor_sampling uniform \
 --nbr_train_distractors $3 --nbr_test_distractors $4 \
---max_sentence_length 20 --vocab_size 100 \
+--max_sentence_length $5 --vocab_size 100 \
 --dataset dSprites \
 --train_test_split_strategy combinatorial2-Y-4-2-X-4-2-Orientation-40-N-Scale-6-N-Shape-3-N \
 --use_cuda \
---batch_size $5 &
+--batch_size $6 &
 #--shared_architecture \
 #--fast \
 
@@ -52,11 +53,11 @@ python train.py --seed $(($1+30)) \
 --epoch 10000 \
 --distractor_sampling uniform \
 --nbr_train_distractors $3 --nbr_test_distractors $4 \
---max_sentence_length 20 --vocab_size 100 \
+--max_sentence_length $5 --vocab_size 100 \
 --dataset dSprites \
 --train_test_split_strategy combinatorial2-Y-4-2-X-4-2-Orientation-40-N-Scale-6-N-Shape-3-N \
 --use_cuda \
---batch_size $5 &
+--batch_size $6 &
 #--shared_architecture \
 #--fast \
 
@@ -66,11 +67,11 @@ python train.py --seed $(($1+40)) \
 --epoch 10000 \
 --distractor_sampling uniform \
 --nbr_train_distractors $3 --nbr_test_distractors $4 \
---max_sentence_length 20 --vocab_size 100 \
+--max_sentence_length $5 --vocab_size 100 \
 --dataset dSprites \
 --train_test_split_strategy combinatorial2-Y-4-2-X-4-2-Orientation-40-N-Scale-6-N-Shape-3-N \
 --use_cuda \
---batch_size $5
+--batch_size $6
 
 #--shared_architecture \
 #--fast \
