a65e8cb4c8d84e649294ebe649761ea3b50af1ec
Denamganai Kevin <denamganai.kevin@gmail.com>
2020-05-29 12:25:24 +0100

    cleaned up examples...

--- examples/experiment2_coordresnet18avgpooled2.sh
+++
@@ -1,5 +0,0 @@
-python example_dSprites_DualLabeled_multi_head_lstm_cnn_agents_splitted_extra.py --seed 10 --arch CoordResNet18AvgPooled-2
-python example_dSprites_DualLabeled_multi_head_lstm_cnn_agents_splitted_extra.py --seed 20 --arch CoordResNet18AvgPooled-2
-python example_dSprites_DualLabeled_multi_head_lstm_cnn_agents_splitted_extra.py --seed 30 --arch CoordResNet18AvgPooled-2
-python example_dSprites_DualLabeled_multi_head_lstm_cnn_agents_splitted_extra.py --seed 40 --arch CoordResNet18AvgPooled-2
-python example_dSprites_DualLabeled_multi_head_lstm_cnn_agents_splitted_extra.py --seed 50 --arch CoordResNet18AvgPooled-2
