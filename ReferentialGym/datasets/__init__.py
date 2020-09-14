e15482092ba3da51f127c4e9fdd1cb29fe6817f3
Kevin Denamganai <denamganai.kevin@gmail.com>
2020-09-14 18:26:23 +0100

    adding 3DShapesPyBullet Dataset.

--- ReferentialGym/datasets/__init__.py
+++ ReferentialGym/datasets/__init__.py
@@ -8,6 +8,7 @@ from .sort_of_CLEVR_dataset import SortOfCLEVRDataset
 from .extended_sort_of_CLEVR_dataset import XSortOfCLEVRDataset
 #from .ah_so_CLEVR_dataset import AhSoCLEVRDataset
 from .spatial_queries_on_object_tuples_dataset import SQOOTDataset 
+from ._3d_shapes_pybullet_dataset import _3DShapesPyBulletDataset 
 
 try:
 	import minerl
