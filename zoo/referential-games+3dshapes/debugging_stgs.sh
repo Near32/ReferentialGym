5cc0ee9c3e46caeb74623692e75596e23881be9d
Kevin Denamganai <denamganai.kevin@gmail.com>
2020-09-19 13:32:22 +0100

    adding stgs agent training scripts...

---
+++ zoo/referential-games+3dshapes/debugging_stgs.sh
@@ -0,0 +1,18 @@
+python -m ipdb -c c train_stgs.py \
+--parent_folder ./debug/Sept16/STGS/\
+--use_cuda --seed 0 \
+--emb_dropout_prob 0.0 --dropout_prob 0.0 --use_sentences_one_hot_vectors \
+--batch_size 50 --mini_batch_size 256 --resizeDim 128 --arch ShortBaselineCNN \
+--descriptive --descriptive_ratio 0.5 \
+--max_sentence_length 20 --vocab_size 5 --epoch 10000 \
+--symbol_processing_nbr_hidden_units 64 --symbol_embedding_size 64 \
+--object_centric --nbr_train_distractors 0 --nbr_test_distractors 0 \
+--agent_loss_type Hinge \
+--metric_fast --metric_epoch_period 20
+
+#--egocentric
+
+#--emb_dropout_prob 0.5 --dropout_prob 0.0 --use_sentences_one_hot_vectors \
+
+#--resizeDim 32 --arch BN+3xCNN3x3
+#--resizeDim 64 --arch BN+BaselineCNN
