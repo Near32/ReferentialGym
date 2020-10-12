71eb591e341acd58b4b41c5384bcbdeddc8c0e81
Kevin Denamganai <denamganai.kevin@gmail.com>
2020-10-12 21:50:31 +0100

    adding &...

--- zoo/referential-games+3dshapes/train_stgs_mlp_gru_discriminative.sh
+++ zoo/referential-games+3dshapes/train_stgs_mlp_gru_discriminative.sh
@@ -14,7 +14,7 @@ python train_stgs_mlp_gru.py \
 --metric_epoch_period 200 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 8 \
 --nb_3dshapespybullet_samples 100 --nb_3dshapespybullet_train_colors 7 \
---lr 6e-4
+--lr 6e-4 &
 
 #--agent_loss_type Hinge \
 #--metric_fast
