1bf59293146a50cf97118ab47a6d7626328d128b
Kevin Denamganai <denamganai.kevin@gmail.com>
2021-05-17 10:06:44 +0100

    updating poorly diverse 3DShapes script with intervention and automatic obverter threshold selection. (+ automatic descriptive ratio selection in train.py, see previous commit actual changes...)

--- zoo/referential-games+compositionality+disentanglement/exp_rg+discr_3DShapes_poorly_diverse_5seeds.sh
+++ zoo/referential-games+compositionality+disentanglement/exp_rg+discr_3DShapes_poorly_diverse_5seeds.sh
@@ -15,11 +15,10 @@
 # $14==CCO (""/"--context_consistent_obverter")
 # $15==DecisionHeadBN (""/"--with_BN_in_obverter_decision_head")
 
-#python -m ipdb -c c RG/zoo/referential-games+compositionality+disentanglement/train.py \
 python -m ipdb -c c train.py \
---parent_folder ./PoorlyDiverseStimuli \
+--parent_folder ./PoorlyDiverseStimuli-May17th \
 --use_cuda --seed $(($1+0)) \
---obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.75 \
+--obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.0 \
 --batch_size $4 --mini_batch_size $4 --vae_lambda 0.0 \
 --resizeDim 64 --arch BN+BetaVAEEncoderOnly3x3 --emb_dropout_prob 0.0 --dropout_prob 0.0 \
 --max_sentence_length $2 --vocab_size $3 \
@@ -27,11 +26,12 @@ python -m ipdb -c c train.py \
 --nbr_train_distractors $6 --nbr_test_distractors $6 \
 --obverter_use_decision_head --obverter_nbr_head_outputs 2 \
 --agent_loss_type $7 --graphtype $8 \
---metric_epoch_period 200 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
+--metric_epoch_period 20 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
 --dis_metric_resampling --metric_resampling --metric_active_factors_only \
 --lr 6e-4 --agent_nbr_latent_dim 32 --vae_nbr_latent_dim 32 \
 --descriptive_ratio $9 \
 ${10} ${11} ${12} ${13} ${14} ${15} \
+--synthetic_progression_end 1000 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 5 --nb_3dshapespybullet_samples 10 --nb_3dshapespybullet_train_colors 3 \
 --dataset 3DShapesPyBullet \
 --train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N &
@@ -39,11 +39,10 @@ ${10} ${11} ${12} ${13} ${14} ${15} \
 #--vae_factor_gamma $5 \
 #--with_baseline \
 
-#python -m ipdb -c c RG/zoo/referential-games+compositionality+disentanglement/train.py \
 python -m ipdb -c c train.py \
---parent_folder ./PoorlyDiverseStimuli \
+--parent_folder ./PoorlyDiverseStimuli-May17th \
 --use_cuda --seed $(($1+10)) \
---obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.75 \
+--obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.0 \
 --batch_size $4 --mini_batch_size $4 --vae_lambda 0.0 \
 --resizeDim 64 --arch BN+BetaVAEEncoderOnly3x3 --emb_dropout_prob 0.0 --dropout_prob 0.0 \
 --max_sentence_length $2 --vocab_size $3 \
@@ -51,22 +50,23 @@ python -m ipdb -c c train.py \
 --nbr_train_distractors $6 --nbr_test_distractors $6 \
 --obverter_use_decision_head --obverter_nbr_head_outputs 2 \
 --agent_loss_type $7 --graphtype $8 \
---metric_epoch_period 200 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
+--metric_epoch_period 20 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
 --dis_metric_resampling --metric_resampling --metric_active_factors_only \
 --lr 6e-4 --agent_nbr_latent_dim 32 --vae_nbr_latent_dim 32 \
 --descriptive_ratio $9 \
 ${10} ${11} ${12} ${13} ${14} ${15} \
+--synthetic_progression_end 1000 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 5 --nb_3dshapespybullet_samples 10 --nb_3dshapespybullet_train_colors 3 \
 --dataset 3DShapesPyBullet \
 --train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N &
+#--descriptive 
 #--vae_factor_gamma $5 \
 #--with_baseline \
 
-#python -m ipdb -c c RG/zoo/referential-games+compositionality+disentanglement/train.py \
 python -m ipdb -c c train.py \
---parent_folder ./PoorlyDiverseStimuli \
+--parent_folder ./PoorlyDiverseStimuli-May17th \
 --use_cuda --seed $(($1+20)) \
---obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.75 \
+--obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.0 \
 --batch_size $4 --mini_batch_size $4 --vae_lambda 0.0 \
 --resizeDim 64 --arch BN+BetaVAEEncoderOnly3x3 --emb_dropout_prob 0.0 --dropout_prob 0.0 \
 --max_sentence_length $2 --vocab_size $3 \
@@ -74,22 +74,23 @@ python -m ipdb -c c train.py \
 --nbr_train_distractors $6 --nbr_test_distractors $6 \
 --obverter_use_decision_head --obverter_nbr_head_outputs 2 \
 --agent_loss_type $7 --graphtype $8 \
---metric_epoch_period 200 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
+--metric_epoch_period 20 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
 --dis_metric_resampling --metric_resampling --metric_active_factors_only \
 --lr 6e-4 --agent_nbr_latent_dim 32 --vae_nbr_latent_dim 32 \
 --descriptive_ratio $9 \
 ${10} ${11} ${12} ${13} ${14} ${15} \
+--synthetic_progression_end 1000 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 5 --nb_3dshapespybullet_samples 10 --nb_3dshapespybullet_train_colors 3 \
 --dataset 3DShapesPyBullet \
 --train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N &
+#--descriptive 
 #--vae_factor_gamma $5 \
 #--with_baseline \
 
-#python -m ipdb -c c RG/zoo/referential-games+compositionality+disentanglement/train.py \
 python -m ipdb -c c train.py \
---parent_folder ./PoorlyDiverseStimuli \
+--parent_folder ./PoorlyDiverseStimuli-May17th \
 --use_cuda --seed $(($1+30)) \
---obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.75 \
+--obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.0 \
 --batch_size $4 --mini_batch_size $4 --vae_lambda 0.0 \
 --resizeDim 64 --arch BN+BetaVAEEncoderOnly3x3 --emb_dropout_prob 0.0 --dropout_prob 0.0 \
 --max_sentence_length $2 --vocab_size $3 \
@@ -97,22 +98,23 @@ python -m ipdb -c c train.py \
 --nbr_train_distractors $6 --nbr_test_distractors $6 \
 --obverter_use_decision_head --obverter_nbr_head_outputs 2 \
 --agent_loss_type $7 --graphtype $8 \
---metric_epoch_period 200 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
+--metric_epoch_period 20 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
 --dis_metric_resampling --metric_resampling --metric_active_factors_only \
 --lr 6e-4 --agent_nbr_latent_dim 32 --vae_nbr_latent_dim 32 \
 --descriptive_ratio $9 \
 ${10} ${11} ${12} ${13} ${14} ${15} \
+--synthetic_progression_end 1000 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 5 --nb_3dshapespybullet_samples 10 --nb_3dshapespybullet_train_colors 3 \
 --dataset 3DShapesPyBullet \
 --train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N &
+#--descriptive 
 #--vae_factor_gamma $5 \
 #--with_baseline \
 
-#python -m ipdb -c c RG/zoo/referential-games+compositionality+disentanglement/train.py \
 python -m ipdb -c c train.py \
---parent_folder ./PoorlyDiverseStimuli \
+--parent_folder ./PoorlyDiverseStimuli-May17th \
 --use_cuda --seed $(($1+40)) \
---obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.75 \
+--obverter_nbr_games_per_round 2 --obverter_threshold_to_stop_message_generation 0.0 \
 --batch_size $4 --mini_batch_size $4 --vae_lambda 0.0 \
 --resizeDim 64 --arch BN+BetaVAEEncoderOnly3x3 --emb_dropout_prob 0.0 --dropout_prob 0.0 \
 --max_sentence_length $2 --vocab_size $3 \
@@ -120,14 +122,16 @@ python -m ipdb -c c train.py \
 --nbr_train_distractors $6 --nbr_test_distractors $6 \
 --obverter_use_decision_head --obverter_nbr_head_outputs 2 \
 --agent_loss_type $7 --graphtype $8 \
---metric_epoch_period 200 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
+--metric_epoch_period 20 --nbr_train_points 150 --nbr_eval_points 100 --metric_batch_size 16 \
 --dis_metric_resampling --metric_resampling --metric_active_factors_only \
 --lr 6e-4 --agent_nbr_latent_dim 32 --vae_nbr_latent_dim 32 \
 --descriptive_ratio $9 \
 ${10} ${11} ${12} ${13} ${14} ${15} \
+--synthetic_progression_end 1000 \
 --nb_3dshapespybullet_shapes 5 --nb_3dshapespybullet_colors 5 --nb_3dshapespybullet_samples 10 --nb_3dshapespybullet_train_colors 3 \
 --dataset 3DShapesPyBullet \
---train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N 
+--train_test_split_strategy combinatorial2-Y-16-S1-X-16-S1-Orientation-4-N-Scale-2-S1-Shape-1-N
+#--descriptive 
 #--vae_factor_gamma $5 \
 #--with_baseline \
 
