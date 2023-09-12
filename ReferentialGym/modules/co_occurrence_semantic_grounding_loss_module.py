695454fd36fd4f3a2d931f582065fe511a35ca24
Denamganai Kevin <denamganai.kevin@gmail.com>
2023-09-12 07:33:10 +0100

    differentiation semantic co-occurrence grounding loss with semantic-level vs sentence-level grounding.

--- ReferentialGym/modules/co_occurrence_semantic_grounding_loss_module.py
+++ ReferentialGym/modules/co_occurrence_semantic_grounding_loss_module.py
@@ -31,7 +31,12 @@ class CoOccurrenceSemanticGroundingLossModule(Module):
         id:str,
         config:Dict[str,object]={
             "lambda_factor":1.0,
+            "sentence_level_lambda_factor":1.0,
             "noise_magnitude":0.0,
+            "semantic_level_grounding": False,
+            "semantic_level_ungrounding": False,
+            "sentence_level_grounding": True,
+            "sentence_level_ungrounding": False,
         },
         input_stream_ids:Dict[str,str]=None,
     ):
@@ -45,6 +50,8 @@ class CoOccurrenceSemanticGroundingLossModule(Module):
             "mode":"signals:mode",
 
             "agent":"modules:current_speaker:ref:ref_agent",
+            "sentences_widx":"modules:current_speaker:sentences_widx",
+            "sentences_logits":"modules:current_speaker:sentences_logits",
             "visual_features":"modules:current_speaker:ref:ref_agent:model:modules:InstructionGenerator:visual_features",
             "text_features":"modules:current_speaker:ref:ref_agent:model:modules:InstructionGenerator:text_features",
             "semantic_prior":"modules:current_speaker:ref:ref_agent:model:modules:InstructionGenerator:semantic_prior",
@@ -67,6 +74,11 @@ class CoOccurrenceSemanticGroundingLossModule(Module):
         
         self.noise_magnitude = self.config.get('noise_magnitude', 0.0)
         self.noisy = self.noise_magnitude > 1.0e-3
+
+        self.semantic_level_grounding = self.config.get('semantic_level_grounding', False)
+        self.semantic_level_ungrounding = self.config.get('semantic_level_ungrounding', False)
+        self.sentence_level_grounding = self.config.get('sentence_level_grounding', False)
+        self.sentence_level_ungrounding = self.config.get('sentence_level_ungrounding', False)
         
     def compute(self, input_streams_dict:Dict[str,object]) -> Dict[str,object] :
         """
@@ -82,7 +94,9 @@ class CoOccurrenceSemanticGroundingLossModule(Module):
         epoch = input_streams_dict["epoch"]
         
         agent = input_streams_dict["agent"]
-        
+        sentences_logits = input_streams_dict["sentences_logits"]
+        # (batch_size x max_sentence_lengths x vocab_size)
+    
         visual_features = input_streams_dict['visual_features']
         # (batch_size x nbr_visual_emb = 1 x emb_size)
         batch_size = visual_features.shape[0]
@@ -106,61 +120,85 @@ class CoOccurrenceSemanticGroundingLossModule(Module):
         noise = 0.0
         targets_logits = (-1)*torch.ones((batch_size, nbr_text_features, nbr_visual_features))
         mask = torch.zeros((batch_size, nbr_text_features, nbr_visual_features))
-        
+        # Sentence level :
+        sentences_targets_logits = torch.zeros((batch_size, nbr_text_features))
+        sentences_mask = torch.zeros((batch_size, nbr_text_features))
+        # Aggregation can be done in many different ways:
+        #sentences_mean_logits = torch.mean(sentences_logits, dim=1, keepdim=False)
+        sentences_mean_logits, _ = torch.max(sentences_logits, dim=1, keepdim=False)
+        # (batch_size x vocab_size)
+        sentences_mean_probs=sentences_mean_logits.softmax(dim=-1)
+        sentences_distr = Categorical(probs=sentences_mean_probs)
+        sentence_level_entropy = sentences_distr.entropy()
+         
         #histogram_tfidx = []
         for tfidx in range(nbr_text_features):
-            for vfidx in range(nbr_visual_features):
-                tfidx_mask = (grounding_signal == tfidx)
-                nontfidx_mask = (grounding_signal != tfidx)
-                tfidx_indices = torch.nonzero(tfidx_mask.sum(-1), as_tuple=True)
-                nontfidx_indices = torch.nonzero(nontfidx_mask.sum(-1), as_tuple=True)
+            tfidx_mask = (grounding_signal == tfidx)
+            nontfidx_mask = (grounding_signal != tfidx)
+            tfidx_indices = torch.nonzero(tfidx_mask.sum(-1), as_tuple=True)
+            nontfidx_indices = torch.nonzero(nontfidx_mask.sum(-1), as_tuple=True)
                 
-                '''
-                # Only address values where something positive occurs.
-                # Otherwise, they are masked out of the loss...
-                if len(tfidx_indices[0]) == batch_size:
-                    # filtering out the text features with zero entropy
-                    # TODO: investigate whether putting a null target could be beneficial?
-                    continue
-                elif len(tfidx_indices[0]) == 0 :   
-                    continue
-                '''
-
-                # Address values when entropy over batch is non null:
-                # i.e. either something positive or negative occurs.
-                # Otherwise, they are masked out of the loss...
-                
-                if len(tfidx_indices[0]) == batch_size:
-                    # filtering out the text features with zero entropy
-                    continue
-                    #
-                    #histogram_tfidx.append(tfidx)
-                    # 
-                    # After examination, it turns out that the only words
-                    # with zero entropy are indeed words that we do not want
-                    # to ground, because either already grounded (e.g. EoS token)
-                    # or not really helping (e.g. 'pick up' from a PickUpDist task).
-                
-                if self.noisy:  noise = torch.rand(1).item()*self.noise_magnitude
+            # Address values when entropy over batch is non null:
+            # i.e. either something positive or negative occurs.
+            # Otherwise, they are masked out of the loss...
+            
+            if len(tfidx_indices[0]) == batch_size:
+                # filtering out the text features with zero entropy
+                continue
+                #
+                #histogram_tfidx.append(tfidx)
+                # 
+                # After examination, it turns out that the only words
+                # with zero entropy are indeed words that we do not want
+                # to ground, because either already grounded (e.g. EoS token)
+                # or not really helping (e.g. 'pick up' from a PickUpDist task).
+            
+            if self.semantic_level_grounding:
+                for vfidx in range(nbr_visual_features):
+                    if self.noisy:  noise = torch.rand(1).item()*self.noise_magnitude
+                    postarget = 1.0-noise
+                    negtarget = -1.0+noise
+                    
+                    if len(tfidx_indices[0]):
+                        mask[tfidx_indices[0], tfidx, vfidx] = 1.0
+                        targets_logits[tfidx_indices[0], tfidx, vfidx] = postarget
+                    if self.semantic_level_ungrounding \
+                    and len(nontfidx_indices[0]):
+                        mask[nontfidx_indices[0], tfidx, vfidx] = 1.0
+                        targets_logits[nontfidx_indices[0], tfidx, vfidx] = negtarget
+            
+            # Sentence level :
+            if self.sentence_level_grounding:
+                if self.noisy:    noise = torch.rand(1).item()*self.noise_magnitude
                 postarget = 1.0-noise
-                negtarget = -1.0+noise
-                
-                if len(tfidx_indices[0]):
-                    mask[tfidx_indices[0], tfidx, vfidx] = 1.0
-                    targets_logits[tfidx_indices[0], tfidx, vfidx] = postarget
-                if len(nontfidx_indices[0]):
-                    mask[nontfidx_indices[0], tfidx, vfidx] = 1.0
-                    targets_logits[nontfidx_indices[0], tfidx, vfidx] = negtarget
-                
-
+                negtarget = noise
+                sentences_targets_logits[tfidx_indices[0], tfidx] = postarget
+                if self.sentence_level_ungrounding:
+                    sentences_targets_logits[nontfidx_indices[0], tfidx] = negtarget
+                sentences_mask[tfidx_indices[0], tfidx] = 1.0
+            
         #wandb.log({f"{mode}/co_occurrence_semantic_grounding/{agent.agent_id}/TokenIdxFullBatch":wandb.Histogram(histogram_tfidx),}, commit=False)
-        targets_logits = targets_logits.to(semantic_prior.device)
-        mask = mask.to(semantic_prior.device)
-        loss = (mask*torch.square(semantic_prior_logits-targets_logits)).mean(-1).mean(-1)
+        if self.semantic_level_grounding:
+            targets_logits = targets_logits.to(semantic_prior.device)
+            mask = mask.to(semantic_prior.device)
+            loss = (mask*torch.square(semantic_prior_logits-targets_logits)).mean(-1).mean(-1)
+        else:
+            loss = torch.zeros_like(semantic_prior).sum(dim=-1)
+        # (batch_size, )
+        
+        # Sentence level :
+        if self.sentence_level_grounding:
+            sentences_targets_logits = sentences_targets_logits.to(sentences_mean_probs.device)
+            sentences_mask = sentences_mask.to(sentences_mean_probs.device)
+            sentences_loss = (sentences_mask*torch.square(sentences_targets_logits-sentences_mean_probs)).mean(-1)
+        else:
+            sentences_loss = torch.zeros_like(loss)
         # (batch_size, )
         
         logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/co_occurrence_semantic_grounding/{agent.agent_id}/Entropy"] = entropy
+        logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/co_occurrence_semantic_grounding/{agent.agent_id}/SentenceLevelEntropy"] = sentence_level_entropy
         losses_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/co_occurrence_semantic_grounding/{agent.agent_id}/Loss"] = [self.config.get("lambda_factor", 1.0), loss]
+        losses_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/co_occurrence_semantic_grounding/{agent.agent_id}/SentenceLevelLoss"] = [self.config.get("sentence_level_lambda_factor", 1.0), sentences_loss]
 
         return outputs_stream_dict
     
