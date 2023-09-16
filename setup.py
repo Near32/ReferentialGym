94df3ef1090f4021009ebe85ded8e5dbecb276eb
Kevin Denamganai <denamganai.kevin@gmail.com>
2023-09-16 07:39:14 +0000

    setup fix

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup(
 	    'scikit-image',
         # previously: 'scikit-learn==0.23',
         # now:
-        'scikit-learn==1.0',
+        #'scikit-learn==1.0',
+        'scikit-learn',
 	    'h5py',
 	    'opencv-python',
 	    #'torch==1.4',
