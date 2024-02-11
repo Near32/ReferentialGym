caught bug with l1/2 regularisation when gradient is too small...

ReferentialGym/networks/networks.py
@@ -57,7 +57,7 @@ def l1_reg(cum_loss_dict, module):
         if 'bias' in name or \
         param is None \
         or not(hasattr(param, 'grad')) or param.grad is None \
-        or torch.norm(param.grad, p=1) < 1e-12 :
+        or torch.norm(param.grad, p=1).item() == 0: # < 1e-20 :
             continue
         cum_loss_dict[name] = torch.norm(param, p=1)
 
@@ -66,7 +66,7 @@ def l2_reg(cum_loss_dict, module):
         if 'bias' in name or \
         param is None \
         or not(hasattr(param, 'grad')) or param.grad is None \
-        or torch.norm(param.grad, p=1) < 1e-12 :
+        or torch.norm(param.grad, p=1).item() == 0 :
             continue
         cum_loss_dict[name] = torch.norm(param, p=2)
 