Solved bug with stgs computation of descriptive accuracy

ReferentialGym/agents/discriminative_listener.py
@@ -173,19 +173,39 @@ def discriminative_st_gs_referential_game_loss(agent,
     target_decision_idx = sample["target_decision_idx"]
     
     if config['descriptive']:
+        #The last element is the not_target logit
         per_stimulus_decision_logits = final_decision_logits.reshape((batch_size, nbr_distractors_po, -1))
+        not_target_logit = per_stimulus_decision_logits[:,-1].unsqueeze(1).repeat(1,nbr_distractors_po-1,1)
+        per_stimulus_decision_logits = torch.cat([
+                per_stimulus_decision_logits[:,:-1],
+                not_target_logit,
+                ],
+                dim=-1,
+        )
         per_stimulus_decision_probs = per_stimulus_decision_logits.softmax(dim=-1)
-        # (batch_size, (nbr_distractors+1), 2)
+        # (batch_size, (nbr_distractors+1)/-1, 2)
         per_stimulus_decision_index = per_stimulus_decision_logits.max(dim=-1, keepdim=False)[1]
         # (batch_size, (nbr_distractors+1))
         per_stimulus_target_index = torch.ones_like(per_stimulus_decision_index)
         # (batch_size, (nbr_distractors+1))
         for bidx in range(batch_size):
-            if target_decision_idx[bidx].long().item() < nbr_distractors_po:
+            if target_decision_idx[bidx].long().item() < nbr_distractors_po-1: #to account for the not_target logit
                 per_stimulus_target_index[bidx, target_decision_idx[bidx].long()] = 0
-        descriptive_accuracy = (per_stimulus_target_index==per_stimulus_decision_index).float()*100.0
+        descriptive_accuracy = (per_stimulus_target_index==per_stimulus_decision_index)#.float()*100.0
         # (batch_size, (nbr_distractors+1))
+        positives = (per_stimulus_target_index==0)
+        descriptive_true_positives = (positives*descriptive_accuracy).float().sum()
+        # (batch_size, (nbr_distractors+1))
+        positives = positives.float().sum()
+        descriptive_accuracy = descriptive_accuracy.float()*100.0
+        descriptive_precision = (descriptive_true_positives/positives).item()
+        true_positives_p_false_negatives = (per_stimulus_decision_index==0).float().sum()
+        descriptive_recall = (descriptive_true_positives/true_positives_p_false_negatives).item()
+        descriptive_f1_score = 2*descriptive_precision*descriptive_recall/(descriptive_precision+descriptive_recall)
         logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/referential_game_descriptive_accuracy"] = descriptive_accuracy.mean(dim=-1)
+        logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/referential_game_descriptive_precision"] = descriptive_precision
+        logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/referential_game_descriptive_recall"] = descriptive_recall
+        logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/referential_game_descriptive_f1_score"] = descriptive_f1_score
         #logs_dict[f"{mode}/repetition{it_rep}/comm_round{it_comm_round}/referential_game_descriptive_prob_target"] = per_stimulus_decision_probs_target.mean(dim=-1)
         outputs_dict["descriptive_accuracy"] = descriptive_accuracy.mean(dim=-1)
     